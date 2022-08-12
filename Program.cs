f1fec892169086e79186ca4090847514b2211e69
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-11 21:28:08 -0300

    adiciona backend com api de rolagem de dados

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "vc tem dado em casa ?");
+
+app.MapGet("/dado/d{NumeroDeFaces}",( [FromRoute] int NumeroDeFaces ) => {
+    if (NumeroDeFaces <= 0)
+    {
+        return Results.BadRequest(new Object{ mensagem = "somente dados com pelo menos uma face"});
+    }
+
+    int face = RandomNumberGenerator.GetInt32(1,NumeroDeFaces + 1);
+
+    return Results.Ok( new { dado = $"d{NumeroDeFaces}", rolagem = face });
+});
+
+app.Run();
